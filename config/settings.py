1588ea1315247b31de4a45c26d6463fbba10c7b1
stanislavbut-design <stanislav.but@gmail.com>
2026-07-18 18:40:20 +0300

    S01-M01.01.01 Master Data Foundation

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.core.apps.CoreConfig'
+    'apps.core',
+    'apps.masterdata'
 ]
 
 MIDDLEWARE = [
